bce138b8236de6826b97f87d4bb3e08c46d8d78d
NoxiusDev <federicoledesma542@gmail.com>
2025-09-01 20:49:53 -0300

    Versiones1

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+createRoot(document.getElementById('root')).render(<App />);
